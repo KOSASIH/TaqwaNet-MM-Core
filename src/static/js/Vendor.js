3bbe163488da677718012c72c787d1165dca2d8a
KOSASIH <kosasihg88@gmail.com>
2024-11-22 18:14:08 +0700

    Create Vendor.js

---
+++ src/static/js/Vendor.js
@@ -0,0 +1,18 @@
+// Vendor.js - Third-party libraries
+
+// jQuery (example)
+(function (global, $) {
+    // jQuery code can be added here if needed
+    $(document).ready(function () {
+        console.log('jQuery is ready!');
+    });
+})(window, jQuery);
+
+// Lodash (example)
+(function (global, _) {
+    // Lodash code can be added here if needed
+    const array ```javascript
+    const array = [1, 2, 3, 4, 5];
+    const shuffledArray = _.shuffle(array);
+    console.log('Shuffled Array:', shuffledArray);
+})(window, _);
